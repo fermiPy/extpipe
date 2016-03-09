5608e2d51cde72db4d1644e16eadf86f4da81329
Matthew Dunseth Wood <mdwood137@gmail.com>
2016-03-09 14:39:53 -0800

    Updates to aggregate script.

--- aggregate.py
+++ aggregate.py
@@ -8,6 +8,7 @@ import argparse
 from astropy.table import Table, Column
 from fermipy.roi_model import ROIModel
 from haloanalysis.utils import collect_dirs
+from haloanalysis.batch import *
 
 def find_source(data):
 
@@ -18,6 +19,26 @@ def find_source(data):
             continue
 
         return s
+
+def find_new_sources(data):
+
+    srcs = sorted(data['sources'].values(), key=lambda t: t['offset'])
+
+    new_srcs = []
+    
+    for s in srcs:
+        if s['SourceType'] == 'DiffuseSource':
+            continue
+
+        if s['offset'] > 1.0:
+            continue
+
+        if '3FGL' in s['name']:
+            continue
+        
+        new_srcs += [s]
+
+    return new_srcs
     
 usage = "usage: %(prog)s"
 description = "Aggregate analysis output."
@@ -82,9 +103,19 @@ cols = [Column([], name='name', dtype='S20', format='%s',description='Source Nam
         Column([], name='ext2_mle', dtype='f8', format='%.3f',unit='deg'),
         Column([], name='ext2_err', dtype='f8', format='%.3f',unit='deg'),
         Column([], name='ext2_ul95', dtype='f8', format='%.3f',unit='deg',description='Extension 95% CL UL (post-localization)'),
+
+        Column([], name='ext_ts', dtype='f8', format='%.2f'),
+        Column([], name='ext_mle', dtype='f8', format='%.3f',unit='deg'),
+        Column([], name='ext_err', dtype='f8', format='%.3f',unit='deg'),
+        Column([], name='ext_ul95', dtype='f8', format='%.3f',unit='deg',description='Extension 95% CL UL (post-localization)'),
+        
         Column([], name='fit1_dlike', dtype='f8', format='%.2f'),
         Column([], name='fit2_dlike', dtype='f8', format='%.2f'),
+        Column([], name='fit3_dlike', dtype='f8', format='%.2f'),
+        Column([], name='fit4_dlike', dtype='f8', format='%.2f'),
+        Column([], name='fit_dlike', dtype='f8', format='%.2f'),
         Column([], name='fit2_nsrc', dtype='i8'),
+        Column([], name='fit_nsrc', dtype='i8'),
         ]
 
 
@@ -92,79 +123,91 @@ tab = Table(cols)
 
 for d in dirs:
 
+    logfile = os.path.join(d,'run_analysis.log')
+
+    if not os.path.isfile(logfile):
+        continue
+
+    if not check_log(logfile)=='Successful':
+        continue
+    
     print d
-    
-    file0 = os.path.join(d,'fit0.npy')
-    file1 = os.path.join(d,'fit1.npy')
-    file2 = os.path.join(d,'fit2.npy')
-    file3 = os.path.join(d,'fit1_halo_data.npy')
-    file4 = os.path.join(d,'fit2_halo_data.npy')
-    file5 = os.path.join(d,'new_source_data.npy')
 
-    if not os.path.isfile(file4):
-        continue
-    
-    if not os.path.isfile(file0) or not os.path.isfile(file1):
-        continue
+    fit_data = []
+    halo_data = []
 
-    try:
-        data0 = np.load(file0).flat[0]
-        data1 = np.load(file1).flat[0]
-        data2 = np.load(file2).flat[0]
-        halo_data1 = np.load(file3)
-        halo_data2 = np.load(file4)
-        new_srcs = np.load(file5)
-    except Exception as e:
-        traceback.print_exc()
-        continue
+    for i in range(5):
+
+        file1 = os.path.join(d,'fit%i.npy'%i)
+        file2 = os.path.join(d,'fit%i_halo_data.npy'%i)
+        if os.path.isfile(file1):
+            fit_data += [np.load(file1).flat[0]]
         
-    src0 = find_source(data0)
-    src1 = find_source(data1)
-    src2 = find_source(data2)
+        if os.path.isfile(file2):
+            halo_data += [np.load(file2)]
+
+#    try:
+#        data0 = np.load(file0).flat[0]
+#        data1 = np.load(file1).flat[0]
+#        data2 = np.load(file2).flat[0]
+#        halo_data1 = np.load(file3)
+#        halo_data2 = np.load(file4)
+#    except Exception as e:
+#        traceback.print_exc()
+#        continue
+
+    src_data = []
+    ext_data = []
+    ext_results = []
+    new_srcs = []
+    
+    for fd in fit_data:
+
+        src = find_source(fd)
+        srcs = find_new_sources(fd)
+        ext = src['extension']
+        src_data += [src]
+        ext_data += [src['extension']]
+        new_srcs += [srcs]
         
-    ext0 = src0['extension']
-    ext1 = src1['extension']
-    ext2 = src2['extension']
-
-    ext0_results = [np.nan,np.nan,np.nan,np.nan]
-    ext1_results = [np.nan,np.nan,np.nan,np.nan]
-    ext2_results = [np.nan,np.nan,np.nan,np.nan]
-
-    if ext0 is not None:
-        ext0_results = [max(ext0['ts_ext'],0),ext0['ext'],ext0['ext_err'],ext0['ext_ul95']]
-
-    if ext1 is not None:
-        ext1_results = [max(ext1['ts_ext'],0),ext1['ext'],ext1['ext_err'],ext1['ext_ul95']]
-
-    if ext2 is not None:
-        ext2_results = [max(ext2['ts_ext'],0),ext2['ext'],ext2['ext_err'],ext2['ext_ul95']]
+        if src['extension'] is None:
+            ext_results += [[np.nan,np.nan,np.nan,np.nan]]
+        else:
+            ext_results += [[max(ext['ts_ext'],0),ext['ext'],ext['ext_err'],ext['ext_ul95']]]
+                        
+    fit1_dlike = fit_data[1]['roi']['logLike'] - fit_data[0]['roi']['logLike']
+    fit2_dlike = fit_data[2]['roi']['logLike'] - fit_data[0]['roi']['logLike']
+    fit_dlike = fit_data[-1]['roi']['logLike'] - fit_data[0]['roi']['logLike']
     
-    fit1_dlike = data1['roi']['logLike'] - data0['roi']['logLike']
-    fit2_dlike = data2['roi']['logLike'] - data0['roi']['logLike']
-    nsrc = len(new_srcs)
+    row = [src_data[0]['assoc']['ASSOC1'],src_data[0]['class'],
+           src_data[1]['ra'],src_data[1]['dec'],src_data[1]['glon'],src_data[1]['glat'],
+           src_data[1]['ts'],src_data[1]['Npred'],
+           src_data[0]['ts'],src_data[1]['ts'],src_data[2]['ts'],
+           src_data[0]['offset'],src_data[1]['offset'],src_data[2]['offset'],
+           src_data[1]['dfde1000'][0],src_data[1]['dfde1000'][1],
+           src_data[1]['dfde1000_index'][0],src_data[1]['dfde1000_index'][1],
+           src_data[1]['flux1000'][0],src_data[0]['flux1000'][1],
+           src_data[1]['eflux1000'][0],src_data[0]['eflux1000'][1],
+           src_data[1]['flux100'][0],src_data[0]['flux100'][1],
+           src_data[1]['eflux100'][0],src_data[0]['eflux100'][1],
+           src_data[1]['SpectrumType']]
+
+    print len(new_srcs[0]), len(new_srcs[1]), len(new_srcs[2])
+    fit2_nsrc = len(new_srcs[2])
+    fit_nsrc = len(new_srcs[-1])
     
-    row = [src0['assoc']['ASSOC1'],src0['class'],
-           src1['ra'],src1['dec'],src1['glon'],src1['glat'],
-           src1['ts'],src1['Npred'],
-           src0['ts'],src1['ts'],src2['ts'],
-           src0['offset'],src1['offset'],src2['offset'],
-           src1['dfde1000'][0],src1['dfde1000'][1],
-           src1['dfde1000_index'][0],src1['dfde1000_index'][1],
-           src1['flux1000'][0],src0['flux1000'][1],
-           src1['eflux1000'][0],src0['eflux1000'][1],
-           src1['flux100'][0],src0['flux100'][1],
-           src1['eflux100'][0],src0['eflux100'][1],
-           src1['SpectrumType']]
+    for i in range(3):
+        row += ext_results[i]
 
-    row += ext0_results
-    row += ext1_results
-    row += ext2_results    
-    row += [fit1_dlike,fit2_dlike,nsrc]
+    row += ext_results[-1]
+    row += [fit1_dlike,fit2_dlike,np.nan,np.nan,fit_dlike,fit2_nsrc,fit_nsrc]
 
+    
+    
     codename = os.path.basename(d)
     linkname = '{%s}'%codename.replace('+','p').replace('.','_')
     
-    for hd in halo_data1:
+    for hd in halo_data[0]:
         colname = 'fit1_halo_%.2f_%.3f_ts'%(np.abs(hd['params']['Index'][0]),
                                             hd['SpatialWidth'])
         if not colname in tab.colnames:
@@ -178,7 +221,7 @@ for d in dirs:
         
         row += [hd['eflux_ul95']]
 
-    for hd in halo_data2:
+    for hd in halo_data[1]:
         colname = 'fit2_halo_%.2f_%.3f_ts'%(np.abs(hd['params']['Index'][0]),
                                             hd['SpatialWidth'])
         if not colname in tab.colnames:
@@ -192,7 +235,7 @@ for d in dirs:
         
         row += [hd['eflux_ul95']]
             
-    tab.add_row([src0['name'],codename,linkname] + row)
+    tab.add_row([src_data[0]['name'],codename,linkname] + row)
 
     
 m = tab['class']==''
