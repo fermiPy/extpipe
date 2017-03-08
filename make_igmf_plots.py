e1a3faa878c05c656c2fdd932ee89940d675629e
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-03-07 17:18:29 -0800

    update to igmf plotting script

--- make_igmf_plots.py
+++ make_igmf_plots.py
@@ -3,9 +3,23 @@ from astropy.table import Table, join
 import numpy as np
 from haloanalysis.utils import load_source_rows
 from haloanalysis.sed import SED
+from matplotlib.offsetbox import AnchoredText
 import matplotlib
 import sys
 import os
+
+def preliminary(ax=None):
+
+    if ax is None:
+        ax = plt.gca()
+    
+    title = AnchoredText('Preliminary', loc=3,
+                         prop={'size':18,'color': 'red'},
+                         pad=0., borderpad=0.75,
+                         frameon=False)
+    ax.add_artist(title)
+
+table_prefix = 'std_psf0123_joint2a'
 
 
 for f in sys.argv[1:]:
@@ -18,10 +32,10 @@ for f in sys.argv[1:]:
     lcoh = tab['lcoh'][0]*np.ones(tab['dloglike'][0].shape)
     dloglike = tab['dloglike'][0]
 
-    tab_ebounds = Table.read('tables/table_std_all_tev_sources_lnl.fits','EBOUNDS')
+    tab_ebounds = Table.read('tables/table_%s_tev_sources_lnl.fits'%table_prefix,'EBOUNDS')
     tab_sed_tev = Table.read('../haloanalysis/data/CompiledTeVSources.fits')
-    tab_casc = join(Table.read('tables/table_std_all_tev_sources_lnl.fits'), Table.read('tables/table_std_all_tev_sources.fits'))
-    tab_casc_sed = Table.read('tables/table_std_all_tev_sources_sed.fits')
+    tab_casc = join(Table.read('tables/table_%s_tev_sources_lnl.fits'%table_prefix), Table.read('tables/table_%s_tev_sources.fits'%table_prefix))
+    tab_casc_sed = Table.read('tables/table_%s_tev_sources_sed.fits'%table_prefix)
 
     source_full = tab['SOURCE_FULL'][0]
 
@@ -33,8 +47,10 @@ for f in sys.argv[1:]:
 
     plt.figure()
 
-    sed_prim0.plot(color='k',marker='o')
-    sed_prim1.plot(color='k',marker='s')
+    
+    
+    sed_prim0.plot(color='grey',marker='s',label='H.E.S.S. (2005-2006)')                   
+    sed_prim1.plot(color='k',marker='o',label='Fermi-LAT')
     #plt.errorbar(sed_prim.ectr/1E6,
     #             sed_prim.ectr*sed_prim.flux,
     #             sed_prim.ectr*sed_prim.flux_err,
@@ -48,55 +64,62 @@ for f in sys.argv[1:]:
 
 
 
-    idxs = np.arange(0,20,3)
+    idxs = np.arange(0,40,5)
 
     for i, idx in enumerate(idxs):
-        plt.plot(sed_prim0.ectr/1E6, sed_prim0.ectr*tab['prim_tev_flux'][0][lcoh_idx,idx][m],
-                 label='log10(B/G) = %.2f'%(igmf[lcoh_idx,idx]),color=matplotlib.cm.jet(float(i)/float(len(idxs))))
 
-        plt.plot(sed_prim1.ectr/1E6, sed_prim1.ectr*tab['prim_flux'][0][lcoh_idx,idx],
-                 #label='B = %.2f'%(igmf[lcoh_idx,idx]),
-                 color=matplotlib.cm.jet(float(i)/float(len(idxs))))
+        efct0 = sed_prim0.ectr**2/sed_prim0.ewidth*1.602177e-06
+        efct1 = sed_prim1.ectr**2/sed_prim1.ewidth*1.602177e-06
+        
+        plt.plot(sed_prim0.ectr/1E6, efct0*tab['prim_tev_flux'][0][lcoh_idx,idx][m],
+                 label='B = $10^{%.1f}$ G'%(igmf[lcoh_idx,idx]),
+                 color=matplotlib.cm.plasma(float(i)/float(len(idxs))))
 
-        plt.plot(sed_prim1.ectr/1E6, sed_prim1.ectr*tab['casc_flux'][0][lcoh_idx,idx],
+        plt.plot(sed_prim1.ectr/1E6, efct1*tab['prim_flux'][0][lcoh_idx,idx],
                  #label='B = %.2f'%(igmf[lcoh_idx,idx]),
-                 color=matplotlib.cm.jet(float(i)/float(len(idxs))),linestyle='--')
+                 color=matplotlib.cm.plasma(float(i)/float(len(idxs))))
+
+        plt.plot(sed_prim1.ectr/1E6, efct1*tab['casc_flux'][0][lcoh_idx,idx],
+                 #label='B = %.2f'%(igmf[lcoh_idx,idx]),
+                 color=matplotlib.cm.plasma(float(i)/float(len(idxs))),linestyle='--')
 
 
         
     plt.gca().set_xscale('log')
     plt.gca().set_yscale('log')
     plt.gca().set_xlabel('Energy [TeV]')
-    plt.gca().set_ylabel('Energy Flux [MeV cm$^{-2}$ $^{-1}$]')
+    plt.gca().set_ylabel('$\\nu\\mathrm{F}_\\nu$ [erg cm$^{-2}$ s$^{-1}$]')
 
-    plt.legend(frameon=False,ncol=2)
-    plt.gca().set_ylim(1E-9,1E-4)
+    plt.legend(frameon=False,ncol=3,prop={'size' : 12})
+    plt.gca().set_ylim(1E-8*1E-6,1E-4*1E-6)
     plt.gca().set_xlim(1E-3,30.)
 
+    preliminary()
+    
     plt.savefig('%s_igmf_flux.png'%prefix)
 
     plt.figure()
 
-    for i, idx in enumerate(idxs):
+    for i, idx in enumerate(idxs[3:]):
         plt.plot(sed_prim1.ectr/1E6, tab['casc_r68'][0][lcoh_idx,idx],
-                 label='B = %.2f'%(igmf[lcoh_idx,idx]),
-                 color=matplotlib.cm.jet(float(i)/float(len(idxs))))
+                 label='B = $10^{%.1f}$ G'%(igmf[lcoh_idx,idx]),
+                 color=matplotlib.cm.plasma(float(i)/float(len(idxs[3:]))))
 
-    plt.plot(sed_prim1.ectr/1E6,np.sqrt((2.85*(sed_prim1.ectr/100.)**-0.8)**2+0.035**2),color='k',label='SOURCE::PSF3')
+    plt.plot(sed_prim1.ectr/1E6,np.sqrt((2.85*(sed_prim1.ectr/100.)**-0.8)**2+0.035**2),color='k',label='P8R2_SOURCE_V6::PSF3')
 
     #print np.sqrt((2.85*(sed_prim1.ectr/100.)**-0.8)**2+0.035**2)
     
-    plt.legend(frameon=False,ncol=2)
+    plt.legend(frameon=False,ncol=2,prop={'size' : 12})
     plt.gca().set_xscale('log')
     plt.gca().set_yscale('log')
 
     plt.gca().set_xlabel('Energy [TeV]')
-    plt.gca().set_ylabel('R68 [deg]')
+    plt.gca().set_ylabel('R$_{68}$ [deg]')
 
-    plt.gca().set_ylim(1E-3,10.)
-    plt.gca().set_xlim(1E-3,30.)
+    plt.gca().set_ylim(1E-2,10.)
+    plt.gca().set_xlim(1E-3,1.)
 
-
+    preliminary()
     
     plt.savefig('%s_igmf_r68.png'%prefix)
     #plt.figure()
@@ -114,14 +137,23 @@ for f in sys.argv[1:]:
     dloglike -= dloglike[-1,-1]
     
     plt.figure()
-    plt.pcolormesh(lcoh,igmf,dloglike,cmap='BuGn')
+    plt.pcolormesh(10**lcoh,10**igmf,dloglike,cmap='BuGn_r',vmax=0.0,vmin=-64)#,shading='gouraud')
     cb = plt.colorbar()
     cb.set_label('Delta-LogLikelihood')
-    plt.contour(lcoh,igmf,dloglike,
-                levels=[-3.0,3.0],colors=['k'])
+    plt.contour(10**lcoh,10**igmf,dloglike,
+                levels=[-3.0,3.0],colors=['k'],label='test')
+    plt.gca().set_xscale('log')
+    plt.gca().set_yscale('log')
 
-    plt.gca().set_xlabel('log10(Lcoh/Mpc)')
-    plt.gca().set_ylabel('log10(B/G)')
+    plt.plot(0,0,label='95% C.L. Exclusion Region',color='k',linestyle='--')
+
+    plt.gca().set_xlabel('L$_{\mathrm{coh}}$ [Mpc]')
+    plt.gca().set_ylabel('B$_{\mathrm{IGMF}}$ [G]')
+
+    plt.gca().legend(frameon=True,loc='lower right',prop={'size' : 12})
+    
+    preliminary()
+    
     plt.savefig('%s_igmf_constraints.png'%prefix)
 
-    plt.close('all')
+    #plt.close('all')
