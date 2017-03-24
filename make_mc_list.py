eab5d05e51fd9b449c52da99a13dfc54b223c610
Matthew Dunseth Wood <mdwood137@gmail.com>
2017-03-24 15:52:46 -0700

    outstanding changes to sourcelist scripts

--- make_mc_list.py
+++ make_mc_list.py
@@ -4,25 +4,22 @@ import itertools
 import yaml
 from fermipy.utils import *
 
-from fermipy.roi_model import ROIModel
-
-min_prefactor = 1E-14*10**0.5
+min_prefactor = 1E-14
 
 testsource = {'name' : 'testsource', 'Index' : 2.0, 'Prefactor' : min_prefactor,
               'Scale' : 3162., 'glon' : 90.0, 'glat' : 50.0, 'SpatialModel' : 'PSFSource'}
 
 config = {
     'selection' : {'glon' : 90.0, 'glat' : 50.0},
-    'model' : { 'sources' : [testsource] },
+    'model' : { 'sources' : [testsource], 'catalogs' : [] },
     'mc' : {'seed' : 0 }
     }
 
 
 srcs_dict = {}
-#index = np.linspace(1.5,3.0,16)
 index = [1.5,2.0,2.5,3.0]
 amp_scale = np.linspace(0,2,5)
-seeds = np.arange(0,20)
+seeds = np.arange(0,10)
 configs = {}
 
 for t, amp, s in itertools.product(index,amp_scale,seeds):
