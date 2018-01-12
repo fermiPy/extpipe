cfd4d855a1424f0a89a7f444efc90fd77c187b56
Matthew Dunseth Wood <mdwood137@gmail.com>
2018-01-12 15:57:29 -0800

    bug fixes

--- extpipe/scripts/aggregate.py
+++ extpipe/scripts/aggregate.py
@@ -1333,6 +1333,7 @@ def main():
     parser.add_argument('--output', default = 'test.fits')
     parser.add_argument('--suffix', default = '')
     parser.add_argument('--batch', action='store_true', default=False)
+    parser.add_argument('--files_per_job', type=int, default=100)
     parser.add_argument('dirs', nargs='+', default = None,
                         help='Run analyses in all subdirectories of this '
                         'directory.')    
@@ -1340,15 +1341,17 @@ def main():
 
     if args.batch:
 
-        files []
-        for d in dirs:
+        prefix = args.dirs[0]
+        files = []
+        for d in args.dirs:
             files += sorted(glob.glob(os.path.join(d,'*')))
         files = [f for f in files if os.path.isdir(f)]
-        files_per_job = 100
-        njob = len(files)/files_per_job+1
+        njob = len(files)/args.files_per_job+1
 
+        print(files)
+        
         for i in range(njob):
-            job_files = ' '.join(files[i*files_per_job:(i+1)*files_per_job])
+            job_files = ' '.join(files[i*args.files_per_job:(i+1)*args.files_per_job])
 
             outfile = os.path.join(prefix,'table_%s_%02i.fits'%(prefix,i))
             outlog = os.path.splitext(outfile)[0] + '.log'
@@ -1356,6 +1359,7 @@ def main():
             cmd = 'bsub -W 500 -R "bullet,kiso,hequ" '
             cmd += '-oo %s extpipe-aggregate %s --output=%s'%(outlog, job_files,
                                                                outfile)
+            print(cmd)
             os.system(cmd)         
     else:    
         aggregate(args.dirs,args.output,args.suffix)
